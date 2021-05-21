8aa8f3b32991d5d2872288ff478c1a72547858d8
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-05-21 16:42:20 +0200

    Add tests

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -99,7 +99,11 @@ class User implements UserInterface
 
     public function getRoles(): array
     {
-        return array($this->getRole());
+        $roles = [$this->role];
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function getRole(): string
